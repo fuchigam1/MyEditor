6c9936b1af396d90bf94cf2a9206497a729ee57a
arata <contact@materializing.net>
2015-02-28 21:54:38 +0900

    add 初回インストール時、ユーザーグループにアクセス権限を付与するようにした
    アクセス権限がないと、ツールバーのエディタ切替えが表示されないため

--- Config/init.php
+++ Config/init.php
@@ -36,3 +36,48 @@ $this->Plugin->initDb('plugin', 'MyEditor');
 			}
 		}
 	}
+	
+/**
+ * ツールバーのエディター切替え表示のため、ユーザーグループに権限を作成する
+ *   ・権限データがないグループのデータのみ作成する
+ * 
+ */
+	App::uses('Model', 'UserGroup');
+	$UserGroupModel = new UserGroup();
+	$userGroupDataList = $UserGroupModel->find('all', array('recursive' => -1));
+	if ($userGroupDataList) {
+		App::uses('Model', 'Permission');
+		$PermissionModel = new Permission();
+		foreach ($userGroupDataList as $key => $userGroupData) {
+			$id = $userGroupData['UserGroup']['id'];
+			// 管理グループの場合は必要ないので権限追加処理をスキップする
+			if ($id == 1) {
+				continue;
+			}
+			// グループ別のアクセス制限設定を取得する
+			$permissionAuthPrefix = $PermissionModel->UserGroup->getAuthPrefix($id);
+			$permissions = $PermissionModel->find('all', array('conditions' => array(
+				'Permission.user_group_id' => $id
+			)));
+			// MyEditor用権限の存在をチェックする
+			$judgeExists = false;
+			foreach ($permissions as $perm) {
+				if ($perm['Permission']['url'] == '/'. $permissionAuthPrefix . '/my_editor/*') {
+					$judgeExists = true;
+				}
+			}
+			$saveData = array();
+			if (!$judgeExists) {
+				//「MyEditor権限」を追加する
+				$saveData['Permission']['url'] = '/'. $permissionAuthPrefix . '/my_editor/*';
+				$saveData['Permission']['name'] = 'マイエディター権限';
+				$saveData['Permission']['user_group_id'] = $id;
+				$saveData['Permission']['auth'] = true;
+				$saveData['Permission']['status'] = true;
+				$saveData['Permission']['no'] = $PermissionModel->getMax('no', array('user_group_id' => $id)) + 1;
+				$saveData['Permission']['sort'] = $PermissionModel->getMax('sort', array('user_group_id' => $id)) + 1;
+				$PermissionModel->create($saveData);
+				$PermissionModel->save($saveData, false);
+			}
+		}
+	}
